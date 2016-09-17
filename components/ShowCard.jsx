812a0ab57c48b3a4d979b2f916624aa70e40c804
Matt <aftalion.matt@gmail.com>
2016-09-17 14:51:40 -0700

    Created Card Component

---
+++ components/ShowCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+export default const ShowCard = (props) => (
+  <div className='show-card'>
+    <img src={`public/img/posters/${props.poster}`} className='show-card-img' />
+    <div className='show-card-text'>
+      <h3 className='show-card-title'>{props.title}</h3>
+      <h4 className='show-card-year'>({props.year})</h4>
+      <p className='show-card-description'>{props.description}</p>
+    </div>
+  </div>
+)
+
+const { string } = React.PropTypes
+
+ShowCard.propTypes = {
+  title: string.isRequired,
+  description: string.isRequired,
+  year: string.isRequired,
+  poster: string.isRequired
+}
