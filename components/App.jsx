b70ce843495baa5eb6024523b86a6ff566a8c887
Matt <aftalion.matt@gmail.com>
2016-09-16 23:45:50 -0700

    Set up react router

--- components/App.jsx
+++ components/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import {Router, Route, IndexRoute, hashHistory} from 'react-router';
+import Layout from './Layout';
+import Landing from './Landing';
+import Search from './Search';
+import {shows} from '../public/data';
+
+
 
 class App extends React.Component {
   render() {
     return (
-      <div>Hello World</div>
+      <Router history={hashistory}>
+        <Route path='/' component={Layout}>
+            <IndexRoute component={Landing} />
+            <Route path='/search' component={Search} shows={shows} />
+        </Route>
+      </Router>
     )
   }
 }
 
-ReactDom.render(<App />, document.getElementById('app'));
+ReactDom.render(<App />,
+  document.getElementById('app'));
