f5568f3a7d5e49f04ffff3d5072bdfa0cb39da60
Matt <aftalion.matt@gmail.com>
2016-09-17 19:25:54 -0700

    Expanded Store

--- redux/Store.jsx
+++ redux/Store.jsx
@@ -1,14 +1,36 @@
 import redux from 'redux';
-import reactReduc from 'react-redux';
+import reactRedux from 'react-redux';
 
 
 const SET_SEARCH_TERM = 'setSearchTerm';
+const initialState = {
+  searchTerm: ''
+};
 
-const reducer = (state, action) => {
+const rootReducer = (state=initialState, action) => {
   switch(action.type) {
     case SET_SEARCH_TERM:
-    const newState = {};
-    Object.assign(newState, state, {searchTerm: action.value});
-    return newState;
+      return reduceSearchTerm(state, action);
+    default:
+      return state;
+  }
+};
+
+export const reduceSearchTerm = (state, action) => {
+  const newState = {};
+  Object.assign(newState, state, {searchTerm: action.value});
+  return newState;
+};
+
+const store = redux.createStore(rootReducer);
+
+const mapStateToProps = (state) => ({searchTerm: state.searchTerm});
+const mapDispatchToProps = (dispatch) => {
+  return{
+    setSearchTerm(searchTerm) {
+      dispatch({type: SET_SEARCH_TERM, value: searchTerm})
+    }
   }
 }
+
+export const connector = reactRedux.connect(mapStateToProps, mapDispatchToProps);
