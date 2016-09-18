01150b8f95242c6dead3b9ed612917328e3348b7
Matt <aftalion.matt@gmail.com>
2016-09-18 11:02:35 -0700

    ShowCards link to details component

--- components/ShowCard.jsx
+++ components/ShowCard.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 
 const ShowCard = (props) => (
-  <div className='show-card'>
-    <img src={`public/img/posters/${props.poster}`} className='show-card-img' />
-    <div className='show-card-text'>
-      <h3 className='show-card-title'>{props.title}</h3>
-      <h4 className='show-card-year'>({props.year})</h4>
-      <p className='show-card-description'>{props.description}</p>
+  <Link to={`/details/${props.imdbID}`}>
+    <div className='show-card'>
+      <img src={`public/img/posters/${props.poster}`} className='show-card-img' />
+      <div className='show-card-text'>
+        <h3 className='show-card-title'>{props.title}</h3>
+        <h4 className='show-card-year'>({props.year})</h4>
+        <p className='show-card-description'>{props.description}</p>
+      </div>
     </div>
-  </div>
+  </Link>
 )
 
 
