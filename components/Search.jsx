f4ef68ccb1c8ac5ed506d69c2cc0c1a325b31451
Matt <aftalion.matt@gmail.com>
2016-09-17 19:50:25 -0700

    edited for redux, revert to stateless component

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,39 +1,24 @@
 import React from 'react';
 import Header from './Header';
 import ShowCard from './ShowCard';
+import {connector} from '../redux/Store'
 
-export default class Search extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      searchTerm: ''
-    }
-    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
-  }
-  handleSearchTermChange (searchTerm) {
-    this.setState({ searchTerm })
-  }
-
-  render() {
-    return (
-      <div className='container'>
-        <Header
-          handleSearchTermChange={this.handleSearchTermChange}
-          searchTerm={this.state.searchTerm}
-          showSearch
-        />
-        <div className='shows'>
-          {this.props.route.shows
-            .filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
-            .map((show) => (
-              <ShowCard {...show} key={show.imdbID} />
-          ))}
-        </div>
-      </div>
-    )
-  }
-}
+const Search = () => (
+  <div className='container'>
+    <Header showSearch />
+    <div className='shows'>
+      {this.props.route.shows
+        .filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.props.searchTerm.toUpperCase()) >= 0)
+        .map((show) => (
+          <ShowCard {...show} key={show.imdbID} />
+      ))}
+    </div>
+  </div>
+)
 
 Search.propTypes = {
-  route: React.PropTypes.object
+  route: React.PropTypes.object,
+  searchTerm: React.PropTypes.string
 };
+
+export default connector(Search);
