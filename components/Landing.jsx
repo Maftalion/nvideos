171d3267194d94fe8ff3f796bdc7a721008cbbe8
Matt <aftalion.matt@gmail.com>
2016-09-18 20:56:44 -0700

    refactored to requirejs, changed to browserhistory

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import {Link} from 'react-router';
-import {connector} from '../redux/Store';
-import {hashHistory} from 'react-router';
+const React = require('react');
+const {browserHistory} = require('react-router');
+const {Link} = require('react-router');
+const {connector} = require('../redux/Store');
 
 
 class Landing extends React.Component {
@@ -11,36 +11,30 @@ class Landing extends React.Component {
     this.handleSearchTermEvent = this.handleSearchTermEvent.bind(this);
     this.gotoSearch = this.gotoSearch.bind(this);
   }
-
-  handleSearchTermEvent(event) {
+  handleSearchTermEvent (event) {
     this.props.setSearchTerm(event.target.value);
   }
-  gotoSearch(event) {
-    hashHistory.push('search');
+  gotoSearch (event) {
+    browserHistory.push('search');
     event.preventDefault();
   }
-
-  render() {
-    return(
+  render () {
+    return (
       <div className='home-info'>
-        <h1 className='title'>nvideos</h1>
+        <h1 className='title'>nVideos</h1>
         <form onSubmit={this.gotoSearch}>
-          <input
-            value={this.props.searchTerm}
-            onChange={this.handleSearchTermEvent}
-            className='search'
-            type='text'
-            placeholder='Search' />
+          <input value={this.props.searchTerm} onChange={this.handleSearchTermEvent} className='search' type='text' placeholder='Search' />
         </form>
-        <Link to='/search' className='browse-all'>Browse</Link>
+        <Link to='/search' className='browse-all'> or Browse All</Link>
       </div>
     )
   }
-}
+};
+
 
 Landing.propTypes = {
   searchTerm: React.PropTypes.string,
   setSearchTerm: React.PropTypes.func
-}
+};
 
-export default connector(Landing);
+module.exports = connector(Landing);
