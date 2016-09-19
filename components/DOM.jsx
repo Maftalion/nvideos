ef3e1832525e8d01dc5a05df847af76e81f71fc1
Matt <aftalion.matt@gmail.com>
2016-09-18 20:38:47 -0700

    fixed styling

--- components/DOM.jsx
+++ components/DOM.jsx
@@ -1,11 +1,11 @@
-const React = require('react')
-const ReactDOM = require('react-dom')
-const App = require('./App')
-const { match } = require('react-router')
+const React = require('react');
+const ReactDOM = require('react-dom');
+const App = require('./App');
+const {match} = require('react-router');
 
 match({ history: App.History, routes: App.Routes }, (error, redirectLocation, renderProps) => {
   if (error) {
-    return console.error('DOM error', error)
+    return console.error('DOM error', error);
   }
   ReactDOM.render(<App {...renderProps} />, document.getElementById('app'))
-})
+});
